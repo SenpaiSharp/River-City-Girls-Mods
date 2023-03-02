74f516f024f0ad114bc2075dc65064e197cfdf00
SenpaiSharp <36146858+SenpaiSharp@users.noreply.github.com>
2023-03-02 00:01:44 -0500

    River City Girls 2 mods for MelonLoader
    init

---
+++ River City Girls 2/Remove Books from Quick Items/RemoveBooksFromQuickItems.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using HarmonyLib;
+using MelonLoader;
+using RCG.ResourceMaps;
+using RCG.Rollback.Components;
+using RCG.UI.Screens.Widgets;
+using System.Collections.Generic;
+using Tools.BinaryRollback;
+
+namespace RCG2Mods
+{
+    #region MelonLoader
+    /// <summary>
+    /// This Mod for RCG2 removes any Books in your inventory from the Quick Items Widget.
+    /// </summary>
+    public class RemoveBooksFromQuickItems : MelonMod
+    {
+        #region Hacks
+        //HACK: Would like to remove the checks involving this. I think Harmony has better ways of doing this, I just haven't learned them yet.
+        /// <summary>
+        /// Determines if this mod should check for books, since most times it fires off is not related to our checks.
+        /// </summary>
+        internal static bool CatchAddEnabled = false;
+        #endregion
+
+        #region DataMap Hash Values (May change with RCG2 updates)
+        internal const int JoysofToysHash = 440075120;
+        internal const int JockJournal = -1802835302;
+        internal const int Thrillhouse = 1937253779;
+        internal const int Funstruction = 1883562947;
+        #endregion
+    }
+    #endregion
+
+    #region Harmony Patches
+    /// <summary>
+    /// Harmony System.Collections.Generic.List Patch that catches Books and prevents them from being edded.
+    /// </summary>
+    [HarmonyPatch(typeof(List<InventoryData>))]
+    [HarmonyPatch("Add", typeof(InventoryData))]
+    class AddPatch1
+    {
+        //HACK:  I don't really like this class. I don't know enough about Harmony yet and anything involving Generics is bit unpredictable and unreliable. For the purposes of this mod, it seems to work but I would like to change this out once I'm more knowledable.
+
+        #region Harmony Fixes
+        /// <summary>
+        /// Checks if the item being added is a Book and prevents it from adding to the list.
+        /// </summary>
+        /// <param name="__instance">List of Inventory being queued up.</param>
+        /// <param name="item">The Inventory being added to the list.</param>
+        /// <returns>False if the item is a book, preventing the List.Add fucntion from being called. Otherwise True to allow.</returns>
+        static bool Prefix(List<InventoryData> __instance, InventoryData item)
+        {
+            // Check if we're checking for Books during the Quick Items widget functions.
+            if (RemoveBooksFromQuickItems.CatchAddEnabled)
+            {
+                // For some reason, the very first time the game checks for Quick Item Inventory, the list comes back with some errenous objects. Make sure this an object we want.
+                var inv = item as ConsumableData;
+                if (inv != null)
+                {
+                    // Check the item against the known book hashes. If it's one of them, we don't want to add.
+
+                    return (inv.NameHash != RemoveBooksFromQuickItems.Funstruction)
+                        && (inv.NameHash != RemoveBooksFromQuickItems.JockJournal) 
+                        && (inv.NameHash != RemoveBooksFromQuickItems.JoysofToysHash)
+                        && (inv.NameHash != RemoveBooksFromQuickItems.Thrillhouse);
+                  
+                }
+            }
+
+            // Run List.Add as normal.
+            return true;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Harmony Game_Player_QuickItem.UpdateIteration Patch that controls when this mod should be active.
+    /// </summary>
+    [HarmonyPatch(typeof(UI_Game_Player_QuickItem))]
+    [HarmonyPatch("UpdateIteration", typeof(PlayerControllerEntity), typeof(SimulationIteration))]
+    class UpdateIterationPatch1
+    {
+        #region Harmony Fixes
+        /// <summary>
+        /// Enables CatchAdd
+        /// </summary>
+        /// <returns>Always returns true to allow List.Add to run.</returns>
+        static bool Prefix()
+        {
+            return RemoveBooksFromQuickItems.CatchAddEnabled = true;
+        }
+
+        /// <summary>
+        /// Disables CatchAdd
+        /// </summary>
+        static void Postfix()
+        {
+            RemoveBooksFromQuickItems.CatchAddEnabled = false;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Harmony Game_Player_QuickItem.AnimateCycle Patch that controls when this mod should be active.
+    /// </summary>
+    [HarmonyPatch(typeof(UI_Game_Player_QuickItem))]
+    [HarmonyPatch("AnimateCycle", typeof(bool), typeof(PlayerControllerEntity))]
+    class AnimateCyclePatch1
+    {
+        #region Harmony Fixes
+        /// <summary>
+        /// Enables CatchAdd
+        /// </summary>
+        /// <returns>Always returns true to allow List.Add to run.</returns>
+        static bool Prefix()
+        {
+            return RemoveBooksFromQuickItems.CatchAddEnabled = true;
+        }
+
+        /// <summary>
+        /// Disables CatchAdd
+        /// </summary>
+        static void Postfix(UI_Game_Player_QuickItem __instance)
+        {
+            RemoveBooksFromQuickItems.CatchAddEnabled = false;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Harmony Game_Player_QuickItem.UseItem Patch that controls when this mod should be active.
+    /// </summary>
+    [HarmonyPatch(typeof(UI_Game_Player_QuickItem))]
+    [HarmonyPatch("UseItem", typeof(PlayerControllerEntity), typeof(PlayerEntity), typeof(SimulationIteration))]
+    class UseItemPatch1
+    {
+        #region Harmony Fixes
+        /// <summary>
+        /// Enables CatchAdd
+        /// </summary>
+        /// <returns>Always returns true to allow List.Add to run.</returns>
+        static bool Prefix()
+        {
+            return RemoveBooksFromQuickItems.CatchAddEnabled = true;
+        }
+
+        /// <summary>
+        /// Disables CatchAdd
+        /// </summary>
+        static void Postfix()
+        {
+            RemoveBooksFromQuickItems.CatchAddEnabled = false;
+        }
+        #endregion
+    } 
+    #endregion
+}
---
+++ River City Girls 2/Smarter Watch/SmarterWatch.cs
@@ -0,0 +1,106 @@
+﻿using MelonLoader;
+using HarmonyLib; 
+using RCG.Rollback.Components;
+using Tools.BinaryRollback; 
+
+namespace RCG2Mods
+{
+    #region MelonLoader
+    /// <summary>
+    /// This Mod for RCG2 always applies the effects of the Smart Watch, so long as it has been purchased, regardless if it is equipped.
+    /// </summary>
+    public class SmarterWatch : MelonMod
+    {
+        #region DataMap Hash Values (May change with RCG2 updates)
+        internal const int BuffNameHash = 1137501490;
+        internal const int SmartWatchNameHash = -847583387;
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Gets Character Data from the ControllerEntity and PlayerEntity.
+        /// </summary>
+        internal static SaveSlotEntity.CharacterData GetCharacterData(PlayerControllerEntity __instance, PlayerEntity Player)
+        {
+            return __instance.SaveSlot.GetCharacterData(Player.m_baseData.CharacterDescription.NameHash, true);
+        }
+        #endregion
+    } 
+    #endregion
+
+    #region Harmony Patches
+    /// <summary>
+    /// Harmony PlayerControllerEntity.SetEquipment Patch that allows for the removal of the Smart Watch without debuffing the player.
+    /// </summary>
+    [HarmonyPatch(typeof(PlayerControllerEntity))]
+    [HarmonyPatch("SetEquipment", typeof(byte), typeof(int), typeof(SimulationIteration), typeof(PlayerEntity))]
+    class SetEquipmentPatch1
+    {
+        #region Prefixes
+        /// <summary>
+        /// Will catch if the slot being replaced contains the Smart Watch. Continues to original SetEquipment method.
+        /// </summary>
+        /// <param name="__instance">The PlayerControllerEntity calling SetEquipment()</param>
+        /// <param name="slot">The Accessories slot being replaced or set.</param>
+        /// <param name="hash">NameHash of the item being put into the slot.</param>
+        /// <param name="iteration">Active Iteration</param>
+        /// <param name="Player">The controlling player who is having their equipment set.</param>
+        /// <returns>Always returns True to (hopefully) avoid Mod incompatibilites, allowing for other Prefixes.</returns>
+        static bool Prefix(PlayerControllerEntity __instance, byte slot, int hash, SimulationIteration iteration, PlayerEntity Player)
+        {
+            // Apparently happens, judging from the game code.
+            if (Player == null)
+            {
+                return true;
+            }
+
+            // Check if the slot being set already has the Smart Watch.
+            if (slot == 0 && __instance.m_playerData.m_equipmentOne == SmarterWatch.SmartWatchNameHash)
+            {
+                // Remove the Watch before it gets to the main method, avoiding RemoveInstance calls that remove the Buff.
+                __instance.m_playerData.m_equipmentOne = 0;
+
+                // Just to be safe, edit the save slot data too.
+                SaveSlotEntity.CharacterData characterData = SmarterWatch.GetCharacterData(__instance, Player);
+                characterData.m_equipSlot1 = 0;
+            }
+            else if (__instance.m_playerData.m_equipmentTwo == SmarterWatch.SmartWatchNameHash) // Same as above but with slot 2.
+            {
+                __instance.m_playerData.m_equipmentTwo = 0;
+                SaveSlotEntity.CharacterData characterData = SmarterWatch.GetCharacterData(__instance, Player);
+                characterData.m_equipSlot2 = 0;
+            }
+
+            return true;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Harmony PlayerControllerEntity.LoadCharacterDataEntity Patch that buffs the player with the effect of the Smart Watch on character loadup, so long as the Smart Watch is owned.
+    /// </summary>
+    [HarmonyPatch(typeof(PlayerControllerEntity))]
+    [HarmonyPatch("LoadCharacterDataToPlayerEntity", typeof(SimulationIteration), typeof(PlayerEntity))]
+    class LoadCharacterDataToPlayerEntityPatch1
+    {
+        #region Postfixes
+        /// <summary>
+        /// Will apply the Smart Watch buff after all other loading is done.
+        /// </summary>
+        /// <param name="__instance">The PlayerControllerEntity that called SetEquipment()</param>
+        /// <param name="iteration">Active Iteration</param>
+        /// <param name="player">Player to buff.</param>
+        private static void Postfix(ref PlayerControllerEntity __instance, ref SimulationIteration iteration, PlayerEntity player)
+        {
+            // Check and make sure the watch has been acquired.
+            if (__instance.SaveSlot.m_data.m_singletonInventory.Contains(SmarterWatch.SmartWatchNameHash))
+            {
+                // Find the definition and add it.
+                var buff = BuffSystem.BuffDefinition.Find(SmarterWatch.BuffNameHash);
+                player.AddBuff(buff, iteration, player);
+            }
+        } 
+        #endregion
+    } 
+    #endregion
+}
