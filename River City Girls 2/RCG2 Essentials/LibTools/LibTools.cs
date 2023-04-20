e79628707bd72312f18a8c2454c27d560da21360
SenpaiSharp <36146858+SenpaiSharp@users.noreply.github.com>
2023-04-19 23:54:27 -0400

    Create LibTools.cs

---
+++ River City Girls 2/RCG2 Essentials/LibTools/LibTools.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+
+namespace RCG2Mods
+{
+    /// <summary>
+    /// Just initializes some libraries.
+    /// </summary>
+    public class LibTools : MelonPlugin
+    {
+        /* In retrospect, AccessoryCycler and PreFinalizerHooks should have loaded as plugins.
+         * To keep update compatibility, meaning to keep people from having to do anything but overwrite old files
+         * and avoid any conflicting libraries, this plugin will be used to extend plugin like functions to the UserLibs.
+         * Long story short, for now, this is just a way to ensure they get initialized without a bunch of checking.*/
+
+        public override void OnApplicationStarted()
+        { 
+            AccessorySetter.Initialize();
+            PreFinalizerHook.Initialize();
+        }
+    }
+}
